c85f7743424756b8725a44014f41795342249954
William <55791230+GrizzBe@users.noreply.github.com>
2022-01-28 20:47:01 +1300

    First Person Camera

---
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Player_Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GetComponentInChildren<Player_Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aim Camera
+        Vector2 mouseMove = (InputManager.Instance as InputManager).GetMouseDelta();
+        playerCamera.MoveCamera(mouseMove);
+
+        // Shoot Gun
+
+
+    }
+}
